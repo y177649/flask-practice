c2778c14d8942004d7ec6e90bf413f7bdd921855
sato <22311092ys@tama.ac.jp>
2025-06-02 17:38:50 +0900

    update: add db

--- hello.py
+++ hello.py
@@ -1,8 +1,18 @@
 from flask import Flask
 from flask import render_template
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+import pytz
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+db = SQLAlchemy(app)
 
+class Post(db.Model):
+    id = db.Column(db.Integer,primary_key=True)
+    title = db.Column(db.String(50),nullable=False)
+    body = db.Column(db.String(300),nullable=False)
+    created_at = db.Column(db.DateTime,nullable=False,default=datetime.now(pytz.timezone('Asia/Tokyo')))
 
 #最も標準的なルーティング
 @app.route("/")

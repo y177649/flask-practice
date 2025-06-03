bbf6ec29076429859618385ab23f4e652033eb69
sato <22311092ys@tama.ac.jp>
2025-06-03 19:54:31 +0900

    add: サインアップ、ログイン機能の追加、それに伴うhello.pyの変更

--- hello.py
+++ hello.py
@@ -7,16 +7,32 @@ from flask import render_template , request ,redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 import pytz
+from flask_login import login_user, logout_user, login_required, LoginManager, UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
+import os
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+app.config['SECRET_KEY'] = os.urandom(24)
 db = SQLAlchemy(app)
+
+login_manager = LoginManager()
+login_manager.init_app(app)
 
 class Post(db.Model):
     id = db.Column(db.Integer,primary_key=True)
     title = db.Column(db.String(50),nullable=False)
     body = db.Column(db.String(300),nullable=False)
     created_at = db.Column(db.DateTime,nullable=False,default=datetime.now(pytz.timezone('Asia/Tokyo')))
+
+class User(UserMixin,db.Model):
+    id = db.Column(db.Integer,primary_key=True)
+    username = db.Column(db.String(30),unique=True)
+    password = db.Column(db.String(128))
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
 
 #最も標準的なルーティング
 @app.route("/")
@@ -84,12 +100,51 @@ def original():
     return render_template('original.html')
 
 @app.route("/H",methods=['GET','POST'])
+@login_required
 def index():
     if request.method == 'GET':
         posts = Post.query.all()
         return render_template('index.html',posts=posts)
 
+
+@app.route("/signup",methods=['GET','POST'])
+def signup():
+    if request.method == "POST":
+        username = request.form.get('username')
+        password =request.form.get('password')
+
+        user = User(username=username,password=generate_password_hash(password,method='pbkdf2:sha256'))
+
+        db.session.add(user)
+        db.session.commit()
+        return redirect('/login')
+    else:
+        return render_template('signup.html')
+
+
+@app.route("/login",methods=['GET','POST'])
+def login():
+    if request.method == "POST":
+        username = request.form.get('username')
+        password =request.form.get('password')
+
+        user = User.query.filter_by(username=username).first()
+        if check_password_hash(user.password,password):
+            login_user(user)
+            return redirect('/H')
+    else:
+        return render_template('login.html')
+
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect('/login')
+
+
 @app.route("/create",methods=['GET','POST'])
+@login_required
 def create():
     if request.method == "POST":
         title = request.form.get('title')
@@ -104,6 +159,7 @@ def create():
         return render_template('create.html')
 
 @app.route("/<int:id>/edit",methods=['GET','POST'])
+@login_required
 def edit(id):
     post = Post.query.get(id)
     if request.method == "GET":
@@ -117,6 +173,7 @@ def edit(id):
         return redirect('/H')
 
 @app.route("/<int:id>/delete",methods=['GET'])
+@login_required
 def delete(id):
     post = Post.query.get(id)
 
